1fc518ec826975d179bf6a865c514ac60f06e502
idris <idrisdoyin@gmail.com>
2022-03-26 01:10:49 +0100

    write a test for a counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [count, setCount]=useState(0);
+	const [error, setError]=useState('');
+	return (
+		<div className="App" data-test='component-app'>
+			<h1 data-test='counter-display'>Learn react Testing</h1>
+			<h4 data-test='count'>{count}</h4>
+			{
+				error && <h4 data-test='error'>{error}</h4>
+			}
+			<button data-test='increment-button' onClick={()=>{
+				if(error){
+					setError('');
+					setCount(count+1)
+				}else{
+					setCount(count+1)
+				}
+			}}>Increment</button>
+			<button data-test='decrement-button' onClick={()=>{if(count == 0){
+				setError('Count cannot be less than 0');
+			}else{
+				setCount(count-1);
+			}
+			}}>Decrement</button>
+		</div>
+	);
 }
 
 export default App;
