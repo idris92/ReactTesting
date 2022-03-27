459f2cc5542cb71e05344d8b0cb41f3bed92bf25
idris <idrisdoyin@gmail.com>
2022-03-27 02:48:36 +0100

    Start test for guessed words

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -1,10 +1,28 @@
 import React from 'react'
+import propTypes from 'prop-types'
 
-const GuessedWords = ({guessed}) => {
+const GuessedWords = (props) => {
+    let contents
+    if(props.guessWords.length == 0){
+        contents = (
+            <span data-test='guest-instructions'>
+                Try to guess the secret words!
+            </span>
+            )
+    }
     return (
-        <div data-test='guessed'/>
+        <div data-test='component-guessed-word'>{contents}</div>
    
     )
+}
+
+GuessedWords.propTypes={
+    guessWords:propTypes.arrayOf(
+        propTypes.shape({
+            guessWord:propTypes.string.isRequired,
+            letterMatchCount:propTypes.number.isRequired
+        })
+    ).isRequired
 }
 
 export default GuessedWords
--- src/GuessedWords.test.js
+++ src/GuessedWords.test.js
@@ -1,30 +1,47 @@
 import { shallow } from 'enzyme';
 import { findByTestArr } from '../test/test.utils';
 import GuessedWords from './GuessedWords';
+import { checkProps } from '../test/test.utils';
+
+//creating a default props
+const defaultProps = {
+	guessWords: [ { guessWord: 'train', letterMatchCount: 3 } ]
+};
 
 // We decided to leave the setup here instead of doing it in util because we need to pass a props to it
 const setup = (props = {}) => {
-	return shallow(<GuessedWords {...props} />);
+	const setupProps = { ...defaultProps, ...props };
+	return shallow(<GuessedWords {...setupProps} />);
 };
 
+test('Does not throw warning with expected props', () => {
+	checkProps(GuessedWords, defaultProps);
+});
 
-test('check for no error', ()=>{
-    const wrapper=setup();
-    const components=findByTestArr(wrapper,'guessed');
-    expect(components.length).toBe(1);
-})
+test('check for no error', () => {
+	const wrapper = setup();
+	const components = findByTestArr(wrapper, 'component-guessed-word');
+	expect(components.length).toBe(1);
+});
 
-describe("if there are no words guessed", ()=>{
-    test('run without an error', ()=>{
+describe('if there are no words guessed', () => {
+	//Note that the wrapper are used on both cases, so we can make it public and apply to all test in the describe using beforeEach
+	//which execute the code for each test
+	let wrapper;
+	beforeEach(() => {
+		wrapper = setup({ guessWords: [] });
+	});
 
-    });
+	test('run without an error', () => {
+		// const wrapper = setup({guessWords:[]});
+		const components = findByTestArr(wrapper, 'component-guessed-word');
+		expect(components.length).toBe(1);
+	});
 
-    test('renders instructions to guess a word', ()=>{
+	test('renders instructions to guess a word', () => {
+		const instructions = findByTestArr(wrapper, 'guest-instructions');
+		expect(instructions.text().length).not.toBe(0);
+	});
+});
 
-    });
-
-})
-
-describe("if there are words guessed", ()=>{
-
-})
+describe('if there are words guessed', () => {});
