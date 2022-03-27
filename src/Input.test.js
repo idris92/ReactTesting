4aca6a59d77b02c31489c367a4544002d7ad679c
idris <idrisdoyin@gmail.com>
2022-03-27 21:56:35 +0100

    basic input component testing

---
+++ src/Input.test.js
@@ -0,0 +1,15 @@
+import { shallow } from 'enzyme';
+import { findByTestArr } from '../test/test.utils';
+import Input from './Input';
+import { checkProps } from '../test/test.utils';
+
+//We decided to leave the setup here instead of doing it in util because we need to pass a props to it
+const setup = () => {
+	return shallow(<Input />);
+};
+
+test('renders without errors', ()=>{
+    const wrapper=setup();
+    const Inputcomponent=findByTestArr(wrapper, 'input-component');
+    expect(Inputcomponent.length).toBe(1);
+})
