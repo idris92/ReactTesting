ce9ddefb0f657ee6dc40cb747445479a2f851a4b
idris <idrisdoyin@gmail.com>
2022-03-28 12:07:34 +0100

    testing input render for success as true or false

--- src/Input.js
+++ src/Input.js
@@ -1,23 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Input = ({ guessWord }) => {
+const Input = ({ success, guessWord }) => {
 	const [ currentGuess, setCurrentGuess ] = React.useState('');
+	if (success) {
+		return <div data-test="input-component" />;
+	}
 	return (
 		<div data-test="input-component">
 			<form className="form-inline">
 				<input
-					data-test='input-box'
+					data-test="input-box"
 					className="mb-2 mx-sm-3"
 					type="text"
 					placeholder="enter guess"
 					value={currentGuess}
 					onChange={(e) => setCurrentGuess(e.target.value)}
 				/>
-				<button 
-                data-test="submit-button" 
-                onClick={(e)=>e.preventDefault()}
-                className="btn btn-primary mb-2">
+				<button
+					data-test='submit-button'
+					onClick={(e) => {
+						//this is used to prevent default button clicked
+						e.preventDefault();
+						//TODO: update guessword
+						//TODO: check against secret word and update success if needed
+						setCurrentGuess('');
+					}}
+					className="btn btn-primary mb-2"
+				>
 					Submit
 				</button>
 			</form>
--- src/Input.test.js
+++ src/Input.test.js
@@ -5,9 +5,16 @@ import { checkProps } from '../test/test.utils';
 import React from 'react';
 
 //We decided to leave the setup here instead of doing it in util because we need to pass a props to it
-const setup = () => {
-	return shallow(<Input />);
+const setup = (success = false, secretWord = 'party') => {
+	return shallow(<Input success={success} secretWord={secretWord} />);
 };
+
+//Mock module for destructured useState
+// const mockSetCurrentGuess = jest.fn();
+// jest.mock('react', () => ({
+// 	...jest.requireActual('react'),
+// 	useState: (initialState) => [ initialState, mockSetCurrentGuess ]
+// }));
 
 test('renders without errors', () => {
 	const wrapper = setup();
@@ -19,23 +26,107 @@ test('does not through warning with expected props', () => {
 	const expectedProps = { guessWord: 'truth' };
 	checkProps(Input, expectedProps);
 });
-describe("state controlled input field", ()=>{
-    test('state updates with value of input box upon change', ()=>{
-        //created a mock function
-        const mockSetCurentGuess = jest.fn();
-        //created a mock state
-        React.useState=jest.fn(()=>['', mockSetCurentGuess])
+describe('state controlled input field', () => {
+	let wrapper;
+	let mockSetCurentGuess = jest.fn();
+	//this will be used to reset the useState back to its original state.
+	let originalUseState;
+	beforeEach(() => {
+		//This clear the mock function before using it again, to prevent using the previous value
+		mockSetCurentGuess.mockClear();
+		//this keep the original state before being altered
+		originalUseState = React.useState;
+		React.useState = jest.fn(() => [ '', mockSetCurentGuess ]);
+		wrapper = setup();
+	});
+	afterEach(() => {
+		//this reset the state after each operation
+		React.useState = originalUseState;
+	});
+	test('state updates with value of input box upon change', () => {
+		// //created a mock function
+		// //This one and the next will be replaced by the destructured method at the top
+		// const mockSetCurentGuess = jest.fn();
+		// //created a mock state
+		// React.useState = jest.fn(() => [ '', mockSetCurentGuess ]);
 
-        const wrapper=setup();
-        const inputBox=findByTestArr(wrapper, 'input-box')
+		// const wrapper = setup();
+		const inputBox = findByTestArr(wrapper, 'input-box');
 
-        //created a mock event
-        const mockEvent={target:{value:'train'}};
+		//created a mock event
+		const mockEvent = { target: { value: 'train' } };
 
-        //then we simulate a chnge on the inputBox
-        //this simulate an inputBox getting a value of train
-        inputBox.simulate('change',mockEvent);
-        
-        expect(mockSetCurentGuess).toHaveBeenCalledWith('train')
-    })
-})
+		//then we simulate a chnge on the inputBox
+		//this simulate an inputBox getting a value of train
+		inputBox.simulate('change', mockEvent);
+
+		expect(mockSetCurentGuess).toHaveBeenCalledWith('train');
+	});
+
+	test('field is cleared upon submit button click', () => {
+		// const mockSetCurentGuess = jest.fn();
+		// React.useState = jest.fn(() => [ '', mockSetCurentGuess ]);
+		// const wrapper = setup();
+		const submitButton = findByTestArr(wrapper, 'submit-button');
+		//we gave it a preventDefault function with empty brace because there is a preventDefault on the button and the test is expecting an event as well
+		submitButton.simulate('click', { preventDefault() {} });
+		expect(mockSetCurentGuess).toHaveBeenCalledWith('');
+	});
+});
+
+//When success is true and false, which components are showing and which one are hiding, checking for correct behaviours
+describe('renders', () => {
+	describe('success is true', () => {
+		let wrapper;
+		beforeEach(() => {
+			//this set success to be true
+			wrapper = setup(true);
+		});
+		test('Input component renders without errors', () => {
+			const Inputcomponent = findByTestArr(wrapper, 'input-component');
+			expect(Inputcomponent.length).toBe(1);
+		});
+
+		//when success is true, we want to make sure there is no input box or submit button
+		test('Input box does not show', () => {
+			//find the inputBox
+			const inputBox = findByTestArr(wrapper, 'input-box');
+			//expect the input box not to show
+			expect(inputBox.exists()).toBe(false);
+		});
+
+		//test to  make sure submit button does not show when success is true
+		test('Submit does not show', () => {
+			//find submit button
+			const submitButton = findByTestArr(wrapper, 'submit-button');
+			expect(submitButton.exists()).toBe(false);
+		});
+	});
+
+	describe('success is false', () => {
+		let wrapper;
+		beforeEach(() => {
+			//this set success to be true
+			wrapper = setup(false);
+		});
+		test('Input component renders without errors', () => {
+			const Inputcomponent = findByTestArr(wrapper, 'input-component');
+			expect(Inputcomponent.length).toBe(1);
+		});
+
+		//when success is true, we want to make sure there is no input box or submit button
+		test('Input box shows', () => {
+			//find the inputBox
+			const inputBox = findByTestArr(wrapper, 'input-box');
+			//expect the input box not to show
+			expect(inputBox.exists()).toBe(true);
+		});
+
+		//test to  make sure submit button does not show when success is true
+		test('Submit to shows', () => {
+			//find submit button
+			const submitButton = findByTestArr(wrapper, 'submit-button');
+			expect(submitButton.exists()).toBe(true);
+		});
+	});
+});
